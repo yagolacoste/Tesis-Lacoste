069acfccd2f24f949e1b6a1cc65fc939cea04681
svallejos <sebastian.vallejos+github@isistan.unicen.edu.ar>
2022-08-10 15:39:40 -0300

    Don ayudin

--- Bicycle/app/src/main/java/com/Tesis/bicycle/MainActivity.java
+++ Bicycle/app/src/main/java/com/Tesis/bicycle/MainActivity.java
@@ -1,98 +1,42 @@
 package com.Tesis.bicycle;
 
-import static java.util.List.*;
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Looper;
+import android.os.StrictMode;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
-
-import android.os.Build;
-import android.os.Bundle;
-import android.os.StrictMode;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.View;
-import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.location.Priority;
 
-import org.osmdroid.api.IMapController;
-import org.osmdroid.config.Configuration;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapController;
-import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.ItemizedIconOverlay;
-import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
-import org.osmdroid.views.overlay.Marker;
-import org.osmdroid.views.overlay.OverlayItem;
-import org.osmdroid.views.overlay.ScaleBarOverlay;
-import org.osmdroid.views.overlay.compass.CompassOverlay;
-import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
-import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
-import org.osmdroid.views.overlay.gridlines.LatLonGridlineOverlay2;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.IMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class MainActivity extends Activity {
+public class MainActivity extends Activity  {
 
     private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
-    private static final int DEFAULT_UPDATE_INTERVAL = 30;
+    private static final int DEFAULT_UPDATE_INTERVAL = 10; // Puse un valor mas bajo solo para verificar que andaba
     private static final int FAST_UPDATE_INTERVAL = 5;
-
-
-    private MapView map;
-    private MapController myMapController;
-    private MyLocationNewOverlay mLocationOverlay;
-    private CompassOverlay mCompassOverlay;
 
     //Google Api for location services
     private FusedLocationProviderClient fusedLocationProviderClient;
 
-    TextView tv_lat, tv_lon, tv_sensor, tv_update;
-    Switch sw_locationupdates, sw_gps;
-
-    Boolean update = false;
-
+    private TextView tv_lat, tv_lon, tv_sensor, tv_update;
+    private Switch sw_locationupdates, sw_gps;
 
     //Location request is config for all setting related to fusedLocationProviderClient
     private LocationRequest locationRequest;
-
     private LocationCallback locationCallback;
-
-    // La clase LocationSettingsRequest.Builder extiende un Object
-    // y construye una LocationSettingsRequest.
-    private LocationSettingsRequest.Builder builder;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,38 +44,10 @@ public class MainActivity extends Activity {
         StrictMode.setThreadPolicy(policy);
         super.onCreate(savedInstanceState);
 
-//        Context ctx = getApplicationContext();
-//        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
-//
         setContentView(R.layout.activity_main);
-//
-//        map = (MapView) findViewById(R.id.openmapview);
-//        map.setTileSource(TileSourceFactory.MAPNIK);
-//
-//
-//        map.setBuiltInZoomControls(true);
-//        map.setMultiTouchControls(true);
-//        IMapController mapController = map.getController();
-//        mapController.setZoom(15);
-//        GeoPoint startPoint = new GeoPoint(-37.32167, -59.13316);
-//        mapController.setCenter(startPoint);
-//
-//
-//
-//
-//        RotationGestureOverlay mRotationGestureOverlay = new RotationGestureOverlay(ctx, map);
-//        mRotationGestureOverlay.setEnabled(true);
-//        map.setMultiTouchControls(true);
-//        map.getOverlays().add(mRotationGestureOverlay);
-//        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-//
-//
-//        if (checkPermissions(true)) {
-//            InitLayer();
-//
-//
-//        }
 
+        // Mantengo una única instancia de client
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         //Obtener ubicacion con fused location options
         tv_lat = findViewById(R.id.tv_lat);
@@ -141,86 +57,64 @@ public class MainActivity extends Activity {
         sw_gps = findViewById(R.id.sw_gps);
         sw_locationupdates = findViewById(R.id.sw_locationupdates);
 
-
-
         // set all properties of locationRequest
-        locationRequest = new LocationRequest();
+        locationRequest = LocationRequest.create();
         locationRequest.setInterval(1000 * DEFAULT_UPDATE_INTERVAL);
         locationRequest.setFastestInterval(1000 * FAST_UPDATE_INTERVAL);
-        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        locationRequest.setPriority(Priority.PRIORITY_HIGH_ACCURACY); // Por defecto puse HIGH solo para verificar que andaba
 
+        sw_gps.setOnCheckedChangeListener((CompoundButton compoundButton, boolean value) -> {
+            // Viru: Para revisar, creo que no alcanza solo con cambiar el valor
+            // Hay que volver a llamar al requestLocationUpdates o algo asi
+             if (value) {
+                locationRequest.setPriority(Priority.PRIORITY_HIGH_ACCURACY);
+                tv_sensor.setText("Using gps");
+             } else {
+                locationRequest.setPriority(Priority.PRIORITY_BALANCED_POWER_ACCURACY);
+                tv_sensor.setText("using wifi");
+            }
+        });
 
+        sw_locationupdates.setChecked(false);
+        sw_locationupdates.setOnCheckedChangeListener((CompoundButton compoundButton, boolean value) -> {
+            if (value) {
+                startLocationupdates();
+            } else {
+                stopLocationUpdates();
+            }
+        });
+
+        checkPermissions(true);
+
+    }
+
+    private void startLocationupdates() {
         //event that is triggeredwheevr the update internal is met
         locationCallback = new LocationCallback() {
             @Override
             public void onLocationResult(@NonNull LocationResult locationResult) {
                 super.onLocationResult(locationResult);
                 Location location = locationResult.getLastLocation();
-                updateUIValues(location);
+                if (location != null)
+                    updateUIValues(location);
             }
         };
 
-
-        sw_gps.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (sw_gps.isChecked()) {
-                    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-                    tv_sensor.setText("Using gps");
-                } else {
-                    locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-                    tv_sensor.setText("using wifi");
-                }
-            }
-        });
-
-
-        updateGPS();
-
-        sw_locationupdates.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (sw_locationupdates.isChecked()) {
-                    startLocationupdates();
-                } else {
-                    stopLocationupdates();
-                }
-            }
-        });
-
-
-    }
-
-    private void startLocationupdates() {
-        tv_update.setText("Location is being tracked");
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
-            updateGPS();
+        // VIRU: Aca va negada la condicion!
+        if (!(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            tv_update.setText("Location is being tracked");
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback,  Looper.getMainLooper());
+            // VIRU: el updateGps solo obtiene la ubicación una vez
+            // updateGPS();
         }
 
     }
 
-    private void stopLocationupdates() {
+    private void stopLocationUpdates() {
         tv_lat.setText("Not tracking location");
         tv_lon.setText("Not tracking location");
-        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
-    }
-
-    private void updateGPS() {
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
-        if (checkPermissions(true)) {
-            //use the permission
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        //we got permission.Put the values of location. XXX into the UI COmponenet
-                        updateUIValues(location);
-                    }
-                });
-            }
-
-        }
+        if (locationCallback != null)
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback);
     }
 
     private void updateUIValues(Location location){
@@ -230,55 +124,12 @@ public class MainActivity extends Activity {
 
     }
 
-    private void InitLayer() {
-        this.mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this), map);
-        this.map.getOverlays().add(this.mLocationOverlay);
-        this.mLocationOverlay.setEnabled(true);
-        this.mLocationOverlay.enableMyLocation();
-        this.mLocationOverlay.enableFollowLocation();
-
-        this.mCompassOverlay = new CompassOverlay(this, new InternalCompassOrientationProvider(this), map);
-        this.mCompassOverlay.enableCompass();
-        map.getOverlays().add(this.mCompassOverlay);
-    }
-
-
-//    public void onResume() {
-//        super.onResume();
-//        //this will refresh the osmdroid configuration on resuming.
-//        //if you make changes to the configuration, use
-//        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-//        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-//        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
-//    }
-
-//    public void onPause() {
-//        super.onPause();
-//        //this will refresh the osmdroid configuration on resuming.
-//        //if you make changes to the configuration, use
-//        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-//        //Configuration.getInstance().save(this, prefs);
-//        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
-//    }
-
-
     private boolean checkPermissions(boolean request) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-//                || (ContextCompat.checkSelfPermission(this,Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED)
-            if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
-                if (request)
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION, Manifest.permission.ACTIVITY_RECOGNITION}, MainActivity.REQUEST_PERMISSIONS_REQUEST_CODE);
-                return false;
-            }
-        } else {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                if (request)
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MainActivity.REQUEST_PERMISSIONS_REQUEST_CODE);
-                return false;
-            }
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            if (request)
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MainActivity.REQUEST_PERMISSIONS_REQUEST_CODE);
+            return false;
         }
-
-
         return true;
     }
 
@@ -288,9 +139,8 @@ public class MainActivity extends Activity {
 
         if (code == REQUEST_PERMISSIONS_REQUEST_CODE) {
             if (this.checkPermissions(false)) {
-                InitLayer();
-                updateGPS();
-
+                if (this.sw_locationupdates.isChecked())
+                    startLocationupdates();
             }
         }
     }
