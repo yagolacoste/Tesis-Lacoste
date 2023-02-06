a67ae1b92bb051099e8df55f3c68b0ec5015f754
Yago Lacoste <yagolacos@gmail.com>
2023-02-06 10:19:45 -0300

    eureka update

--- Servidor/admin/src/main/java/com/admin/AdminApplication.java
+++ Servidor/admin/src/main/java/com/admin/AdminApplication.java
@@ -2,10 +2,12 @@ package com.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 //@EnableEurekaClient
+@EnableFeignClients
 public class AdminApplication {
 
 	public static void main(String[] args) {
