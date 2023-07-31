68972d6927927e5f4c9d6ce6656a029487e3dd46
Yago Lacoste <ylacoste@temperies.com>
2023-07-31 15:22:02 -0300

    falta clasificacion

--- Bicycle/app/src/main/java/com/Tesis/bicycle/Activity/ui/Fragment/AchievementsFragment.java
+++ Bicycle/app/src/main/java/com/Tesis/bicycle/Activity/ui/Fragment/AchievementsFragment.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 
 import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.Tesis.bicycle.Presenter.Adapter.BattleRecyclerViewAdapter;
 import com.Tesis.bicycle.R;
+import com.Tesis.bicycle.ViewModel.AccessTokenRoomViewModel;
+import com.Tesis.bicycle.ViewModel.StatisticsViewModel;
+
+import java.io.IOException;
 
 public class AchievementsFragment extends Fragment {
 
@@ -18,7 +24,9 @@ public class AchievementsFragment extends Fragment {
 
     private TextView acTextViewCompete,acTextViewSpeed,recordSpeedDate,acTextViewDistance,recordDistanceDate,acTextViewTime,recordTimeDate;
 
+    private AccessTokenRoomViewModel accessTokenRoomViewModel;
 
+    private StatisticsViewModel statisticsViewModel;
     public AchievementsFragment() {
 
     }
@@ -50,6 +58,29 @@ public class AchievementsFragment extends Fragment {
         recordDistanceDate=view.findViewById(R.id.record_distance_date);
         acTextViewTime=view.findViewById(R.id.ac_textView_time);
         recordTimeDate=view.findViewById(R.id.record_time_date);
+        accessTokenRoomViewModel=new ViewModelProvider(this).get(AccessTokenRoomViewModel.class);
+        statisticsViewModel=new ViewModelProvider(this).get(StatisticsViewModel.class);
+        loadDta();
         return view;
     }
+
+    private void loadDta() {
+        accessTokenRoomViewModel.getFirst().observe(getViewLifecycleOwner(),resp->{
+            if(resp.getId()!=null){
+                statisticsViewModel.getAchievements(resp.getId()).observe(getViewLifecycleOwner(),response->{
+                    if(response!=null){
+                        acTextViewCompete.setText(String.valueOf(response.getBattleWinner()));
+                        acTextViewSpeed.setText(String.valueOf(response.getSpeedMax()));
+                        recordSpeedDate.setText(response.getSpeedMaxDate().toString());
+                        acTextViewDistance.setText(String.valueOf(response.getDistanceMax()));
+                        recordDistanceDate.setText(response.getDistanceMaxDate().toString());
+                        acTextViewTime.setText(response.getTimeMin().toString());
+                        recordTimeDate.setText(response.getTimeMinDate().toString());
+                    }
+                });
+            }
+        });
+    }
+
+
 }
